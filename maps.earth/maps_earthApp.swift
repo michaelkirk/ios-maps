0e8e8a2ada70928457ae41d6c7f23c05c7e56fc0
Michael Kirk <michael.code@endoftheworl.de>
2024-01-29 16:30:39 -0800

    Initial Commit

---
+++ maps.earth/maps_earthApp.swift
@@ -0,0 +1,17 @@
+//
+//  maps_earthApp.swift
+//  maps.earth
+//
+//  Created by Michael Kirk on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct maps_earthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
